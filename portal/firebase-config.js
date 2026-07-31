ac6414985789bb67f1ce104f385975f3ba0b6421
Igri Emmanuel <cwiftemmy@gmail.com>
2026-07-31 17:09:54 +0100

    Create firebase-config.js

---
+++ portal/firebase-config.js
@@ -0,0 +1,45 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics"; 
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAgjiykCE4jNOcvn0c5TbGLNs8GLslSYmY",
+  authDomain: "casor-uniport.firebaseapp.com",
+  projectId: "casor-uniport",
+  storageBucket: "casor-uniport.firebasestorage.app",
+  messagingSenderId: "351818447461",
+  appId: "1:351818447461:web:581db762de75ebc825248c",
+  measurementId: "G-DP4D6JLH0Q"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
+// Initialize Firebase
+
